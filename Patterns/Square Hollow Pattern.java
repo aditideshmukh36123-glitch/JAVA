c8b01918ab913714539c9f1baa36e971389a37ad
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-07-10 23:54:03 +0530

    Update Square Hollow Pattern.java

--- Patterns/Square Hollow Pattern.java
+++ Patterns/Square Hollow Pattern.java
@@ -1,1 +1,45 @@
+// *****
+// *   *
+// *   *
+// *   *
+// *****
 
+
+
+
+class SquareHollowPattern{
+
+
+  public static void main(String args[]){
+
+   int i,j,n=5;
+
+    for(i=1;i<=n;i++){
+        
+         for(j=0;j<n;j++){
+
+     if(i==1 || i==n || j==0 || j==n-1 ){
+                            
+       System.out.print("*");
+
+         
+     }else{
+         
+                             
+       System.out.print(" ");
+
+     }
+
+
+         }            
+                     
+                     
+                     
+    System.out.println("");
+
+    }
+
+
+    
+  }
+}
