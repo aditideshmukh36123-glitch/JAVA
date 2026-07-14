1abac0c6e15e0da14a23cfb0a7a1089af9e7f546
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-07-14 09:38:34 +0530

    Update NumberTriangle.java

--- Patterns/NumberTriangle.java
+++ Patterns/NumberTriangle.java
@@ -2,5 +2,20 @@
 
 class NumberTriangle {
     public static void main(String[] args) {
+        
+        int i,j;
+        
+        
+        for(i=1;i<=4;i++){
+            
+            for(j=1;j<=4-i;j++){
+                System.out.print(" ");
+            }
+            
+            for(j=1;j<=i;j++){
+                System.out.print(" "+i);
+            }
+            System.out.println("");
+        }
     }
 }
