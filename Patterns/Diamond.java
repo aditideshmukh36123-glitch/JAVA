09bef30aa8d4113a748b361cbd74174876c42144
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-07-14 20:32:07 +0530

    Update Diamond.java

--- Patterns/Diamond.java
+++ Patterns/Diamond.java
@@ -1,1 +1,48 @@
 
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Diamond{
+
+
+  public static void main(String args[]){
+
+int i,j,n=4;
+
+
+for(i=1;i<=n;i++){
+    
+    
+    for(j=1;j<=n-i;j++){
+        System.out.print(" ");
+    }
+    
+    for(j=1;j<=i;j++){
+        System.out.print(" *");
+    }
+   
+    System.out.println("");
+}
+ 
+ for(i=1;i<=n-1;i++){
+     
+     for(j=1;j<=i;j++){
+         System.out.print(" ");
+     }
+for(j=1;j<=n-i;j++){
+    System.out.print(" *");
+}
+System.out.println("");
+
+ }
+
+
+
+
+
+
+
+
+    
+  }
+}
