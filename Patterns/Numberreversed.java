5b25228340530096cda56c8efa5d0995199ddb9a
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-07-15 23:52:35 +0530

    Implement reverse number pattern printing
    Added a program to print numbers in reverse order.

--- Patterns/Numberreversed.java
+++ Patterns/Numberreversed.java
@@ -1,1 +1,21 @@
-
+// 1 2 3 4 5 6 
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1
+class Main {
+    public static void main(String[] args) {
+        
+        int i,j,n=6;
+        for(i=n;i>=1;i--){
+            
+            for(j=1;j<=i;j++){
+                
+                System.out.print(" "+j);
+            }
+            System.out.println();
+            
+        }
+        
+    }}
