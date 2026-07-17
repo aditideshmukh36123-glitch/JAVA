3ff73d5d6253308a459b36803d6579642ebd5d89
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-07-17 23:12:12 +0530

    Update Number1to10.java

--- Patterns/Number1to10.java
+++ Patterns/Number1to10.java
@@ -1,7 +1,25 @@
 
 
-
 class Number1to10 {
     public static void main(String[] args) {
         
+        
+        
+        
+        int i,j,lines=5,n=1;
+        
+    
+        for(i=1;i<=lines;i++){
+            
+            for(j=1;j<=i;j++){
+                
+                System.out.print(" "+n);
+                  n++;
+            }
+            
+             System.out.println("");
+           
+        }
+        
+    }
 }
