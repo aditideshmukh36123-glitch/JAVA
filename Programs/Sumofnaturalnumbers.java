bf1f2be25d261d327da2f71867617105a36795cd
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-07-20 00:44:05 +0530

    Update Sumofnaturalnumbers.java

--- Programs/Sumofnaturalnumbers.java
+++ Programs/Sumofnaturalnumbers.java
@@ -1,1 +1,19 @@
 
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+Scanner scan=new Scanner(System.in);
+System.out.print("Enter The Number: ");
+int n=scan.nextInt();
+int sum=0;
+
+for(int i=1;i<=n;i++){
+    sum=sum+i;
+
+}
+System.out.println("Sum is: "+sum);
+    }
+}
