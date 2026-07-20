acc2207ef1218f15297cd6589d08fcaaa15a02de
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-07-21 00:09:19 +0530

    Update Rhombuspattern.java

--- Patterns/Rhombuspattern.java
+++ Patterns/Rhombuspattern.java
@@ -1,6 +1,31 @@
-public class Rhombuspattern
+
+
+public class Main
 {
 	public static void main(String[] args) {
+
+
+
+int i,j,n=8;
+
+for(i=1;i<=n;i++){
+    
+    for(j=1;j<=i;j++){
+        
+        System.out.print(" ");
+    }
+    
+    for(j=1;j<=n;j++){
+                System.out.print(" *");
+
+    }
+    
+    
+            System.out.println("");
+
+}
+
+
 
 
 	}
