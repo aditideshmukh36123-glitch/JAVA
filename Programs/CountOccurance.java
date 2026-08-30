a9c84b86e9f39c6f5ab0e61a7208c9c57407ab4a
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-08-30 23:34:08 +0530

    Create CountOccurance.java

---
+++ Programs/CountOccurance.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+
+class CountOccurance {
+    public static void main(String[] args) {
+/*
+
+//Count the occurance of any number entered by user from the number entered by user
+
+*/
+
+Scanner in=new Scanner(System.in);
+System.out.print("Enter a Number: ");
+
+/*
+If the number is too big
+System.out.print("Enter a Number: ");
+BigInteger n = in.nextBigInteger();
+*/
+      
+long num=in.nextLong();
+
+System.out.print("Enter the number you want to count the occurance of : ");
+int f=in.nextInt();
+
+
+int count=0;
+
+long temp=0;
+
+while(num>0){
+
+
+    temp=num%10;
+    num=num/10;
+
+    if(temp==f){
+        count++;
+    }
+}
+
+
+System.out.println(f+" Occured "+count+" Times");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
