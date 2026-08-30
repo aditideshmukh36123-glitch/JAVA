4258158fb4df9cda6a64f1bd5c7fb8b23e958b08
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-08-30 21:07:20 +0530

    Add Largestamongthree.java to find maximum of three numbers
    Implemented a program to find the largest among three numbers using user input.

---
+++ Largestamongthree.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+class Largestamongthree {
+    public static void main(String[] args) {
+
+  //Program to find largest among three
+
+Scanner in=new Scanner(System.in);
+int a,b,c;
+System.out.print("Enter Num1: ");
+a=in.nextInt();
+
+System.out.print("Enter Num2: ");
+b=in.nextInt();
+
+System.out.print("Enter Num3: ");
+c=in.nextInt();
+
+int max=a;
+
+if(b>max){
+max=b;
+}
+if(c>max){
+    max=c;
+}
+
+System.out.println(max);
+
+
+    }
+}
