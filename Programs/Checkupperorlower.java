3d424a70660aa6802a0011d4aa70737a7ae82dd1
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-08-30 21:23:03 +0530

    Create Checkupperorlower.java

---
+++ Programs/Checkupperorlower.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+class Checkupperorlower {
+    public static void main(String[] args) {
+
+
+//check whether the alphabet is uppercase or lower case
+
+System.out.print("Enter to check: ");
+
+Scanner in=new Scanner(System.in);
+
+char ch=in.next().trim().charAt(0);
+
+
+if(ch>='a' && ch<='z'){
+
+    System.out.println("Lowercase");
+}else{
+        System.out.println("Uppercase");
+
+}
+
+
+
+
+
+
+
+    }
+    
+    
+    }
