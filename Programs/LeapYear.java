282f3f10737a8baebfd8d7deebbb1c7fe0946826
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-08-31 00:04:22 +0530

    Add LeapYear program to determine leap years
    Implement a program to check if a year is a leap year based on specific conditions.

---
+++ Programs/LeapYear.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+
+class LeapYear {
+    public static void main(String[] args) {
+
+
+
+
+
+/*
+Leap Year
+
+A year is a leap year if:
+
+1. The year is divisible by 4
+   AND
+2. The year is NOT divisible by 100
+   OR
+3. The year is divisible by 400
+
+Divisible by 4
+      ↓
+   Check 100
+   ↙       ↘
+No         Yes
+↓           ↓
+Leap    Check 400
+            ↓
+       Yes → Leap
+       No  → Not Leap
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+ Scanner in = new Scanner(System.in);
+
+ System.out.print("Enter a Year : ");
+ int year=in.nextInt();
+
+
+if((year%4==0)&&(year%100!=0|| year%400==0)){
+
+    System.out.println("Leap");
+}else{
+        System.out.println("Not Leap");
+
+}
+
+
+
+  }
+    }
