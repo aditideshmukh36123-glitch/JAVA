5abcc6c6b60f7fb67695a5a7148d3abb0a5ece76
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-09-02 19:55:57 +0530

    Create Armstrong.java

---
+++ Programs/Armstrong.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Armstrong {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the Number: ");
+        long num = in.nextLong();
+
+        int count = 0;
+        long temp = num;
+        long digit;
+        long arm = 0;
+
+        // Count number of digits
+        while (temp > 0) {
+            temp = temp / 10;
+            count++;
+        }
+
+        temp = num;
+
+        // Calculate Armstrong value
+        while (temp > 0) {
+
+            digit = temp % 10;
+            temp = temp / 10;
+
+            long power = 1;
+
+            for (int i = 1; i <= count; i++) {
+                power = power * digit;
+            }
+
+            arm = arm + power;
+        }
+
+        System.out.println("Armstrong value: " + arm);
+
+        if (arm == num) {
+            System.out.println("It is an Armstrong number");
+        } else {
+            System.out.println("It is not an Armstrong number");
+        }
+    }
+}
