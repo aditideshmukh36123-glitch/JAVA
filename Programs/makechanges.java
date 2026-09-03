dc85e12159a0eef888b4d20b9d62646d32d77aa0
Aditi Deshmukh <aditideshmukh36123@gmail.com>
2026-09-04 00:41:40 +0530

    Add duplicate digit detection in makechanges.java

---
+++ Programs/makechanges.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        int temp = n;
+
+        while (temp > 0) {
+
+            int digit = temp % 10;
+            int temp2 = temp / 10;
+
+            while (temp2 > 0) {
+
+                int digit2 = temp2 % 10;
+
+                if (digit == digit2) {
+                    System.out.println(digit + " is duplicate");
+                    break;
+                }
+
+                temp2 = temp2 / 10;
+            }
+
+            temp = temp / 10;
+        }
+    }
+}
